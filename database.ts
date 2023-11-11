6dd1bbd67fe408e116274eb970321ffb10dda745
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2023-11-11 13:50:16 +0000

    Add auto-increment to primary key in JD and SAVED_RESUMES tables.

--- database.ts
+++ database.ts
@@ -6,13 +6,13 @@ export async function getDb(): Promise<betterSqlite3.Database> {
     const db = betterSqlite3('./database.db')
 
     // Create a table called JD if it doesn't exist, with following columns, text, url, companyName, post, type, location, date
-    db.prepare('CREATE TABLE IF NOT EXISTS JD (id PRIMARY KEY, text TEXT, url TEXT, companyName TEXT, post TEXT, type TEXT, location TEXT, skills TEXT, softSkills TEXT, date TEXT)').run()
+    db.prepare('CREATE TABLE IF NOT EXISTS JD (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, url TEXT, companyName TEXT, post TEXT, type TEXT, location TEXT, skills TEXT, softSkills TEXT, date TEXT)').run()
 
     // Create a table called saved_resumes if it doesn't exist, with the specified columns
     // Also, add a new column for cover_letter in TEXT format
     db.prepare(`
         CREATE TABLE IF NOT EXISTS SAVED_RESUMES (
-            id INTEGER PRIMARY KEY,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             job_id INTEGER,
             updated_resume TEXT,
             technical_skills TEXT,
