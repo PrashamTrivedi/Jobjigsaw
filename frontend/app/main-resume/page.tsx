1611dff047e7997d84b0912f6c3197fb377c7f5a
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2023-12-10 07:35:58 +0000

    Fixing the page and adding another empty content

--- frontend/app/main-resume/page.tsx
+++ frontend/app/main-resume/page.tsx
@@ -1,4 +1,7 @@
-export function Page() {
+export default function Page() {
     <>
+        <h1 className="text-4xl font-bold text-center dark:text-white">
+            Main Resume
+        </h1>
     </>
 }
