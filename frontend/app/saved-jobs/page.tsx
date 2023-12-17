4093f6c0a328ef2e36c071e0b771993a39a90480
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2023-12-17 12:39:19 +0000

    Adding add new job link in jobs page

--- frontend/app/saved-jobs/page.tsx
+++ frontend/app/saved-jobs/page.tsx
@@ -3,6 +3,8 @@ import {Suspense} from "react"
 import JobLoadingSkeleton from "../ui/JobLoadingSkeleton"
 import JobsList from "../ui/jobsList"
 import {Metadata} from "next"
+import Link from "next/link"
+import {PlusIcon} from "@heroicons/react/20/solid"
 
 export const metadata: Metadata = {
     title: 'Jobs',
@@ -10,7 +12,13 @@ export const metadata: Metadata = {
 export default async function Page() {
     return (
         <>
-            <h1 className="text-4xl text-center">Saved Jobs</h1>
+            <div className="flex items-center w-full justify-center">
+                <h1 className="text-4xl text-center mr-4">Saved Jobs</h1>
+                <Link href='/create-job'
+                    className="mt-3 text-center text-indigo-500 px-4 py-2 rounded" >
+                    Add New Job
+                </Link>
+            </div>
             <Suspense fallback={<JobLoadingSkeleton />}>
                 <JobsList />
             </Suspense >
