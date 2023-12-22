0c66ba1591f3551dd8039caef1b072175ce55022
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2023-12-22 14:57:38 +0000

    Fixed build error

--- jobjigsaw-frontend/src/inferredJob.tsx
+++ jobjigsaw-frontend/src/inferredJob.tsx
@@ -19,7 +19,7 @@ export default function InferredJob(
     const [inferPending, toggleInferPending] = useState(false)
     const [inferMatchPending, toggleInferMatchPending] = useState(false)
 
-    const [viewName, setViewName] = useState('inferrence')
+
 
     async function handleInferJob() {
         toggleInferPending(true)
@@ -39,90 +39,88 @@ export default function InferredJob(
     }
     return (
         <>
-            {viewName === 'inferrence' &&
-                <>
-                    <h1 className='text-4xl font-bold text-center dark:text-white'>
-                        Job Description
-                    </h1>
-                    <p id='jobDescription' className='text-lg mt-4 dark:text-gray-300'>
-                        {jobDescription}
-                    </p>
-                    <h2 className='text-3xl font-bold mt-8 dark:text-white'>
-                        Inferred Job
-                    </h2>
-                    {
-                        !inferredJob || Object.keys(inferredJob).length === 0 ? (
-                            <>
-                                <div className='mt-4'>
-                                    No inferred job found. Click the button below to infer a job.
-                                    <InferJobButton pending={inferPending} onClick={handleInferJob} />
-                                </div >
+            <h1 className='text-4xl font-bold text-center dark:text-white'>
+                Job Description
+            </h1>
+            <p id='jobDescription' className='text-lg mt-4 dark:text-gray-300'>
+                {jobDescription}
+            </p>
+            <h2 className='text-3xl font-bold mt-8 dark:text-white'>
+                Inferred Job
+            </h2>
+            {
+                !inferredJob || Object.keys(inferredJob).length === 0 ? (
+                    <>
+                        <div className='mt-4'>
+                            No inferred job found. Click the button below to infer a job.
+                            <InferJobButton pending={inferPending} onClick={handleInferJob} />
+                        </div >
 
-                            </>
-                        ) :
+                    </>
+                ) :
 
-                            <div className='mt-4'>
-                                <InferredData keyName='Company Name' value={inferredJob.inferredDescription.companyName} />
-                                <InferredData keyName='Job Title' value={inferredJob.inferredDescription?.jobTitle} />
-                                <InferredData keyName='Full Remote?' value={inferredJob.inferredDescription?.isRemote ?? false ? '✅' : '❌'} />
-                                {!(inferredJob.inferredDescription?.isRemote ?? false) &&
-                                    <InferredData keyName='Job Location' value={inferredJob.inferredDescription?.location ?? ""} />
+                    <div className='mt-4'>
+                        <InferredData keyName='Company Name' value={inferredJob.inferredDescription.companyName} />
+                        <InferredData keyName='Job Title' value={inferredJob.inferredDescription?.jobTitle} />
+                        <InferredData keyName='Full Remote?' value={inferredJob.inferredDescription?.isRemote ?? false ? '✅' : '❌'} />
+                        {!(inferredJob.inferredDescription?.isRemote ?? false) &&
+                            <InferredData keyName='Job Location' value={inferredJob.inferredDescription?.location ?? ""} />
 
-                                }
-                                <InferredData keyName='Technical Skills' value={inferredJob.inferredDescription.technicalSkills.join(', ')} />
-                                <InferredData keyName='Soft Skills' value={inferredJob.inferredDescription.softSkills.join(', ')} />
-                                <InferredData keyName='Suger Coating Rating' value={inferredJob.inferredDescription.sugercoatingRating} />
-                                <InferredData keyName='Suger Coating Rating Reason' value={inferredJob.inferredDescription.sugercoatingRatingReason} />
+                        }
+                        <InferredData keyName='Technical Skills' value={inferredJob.inferredDescription.technicalSkills.join(', ')} />
+                        <InferredData keyName='Soft Skills' value={inferredJob.inferredDescription.softSkills.join(', ')} />
+                        <InferredData keyName='Suger Coating Rating' value={inferredJob.inferredDescription.sugercoatingRating} />
+                        <InferredData keyName='Suger Coating Rating Reason' value={inferredJob.inferredDescription.sugercoatingRatingReason} />
 
 
-                                <div className='mt-4'>
-                                    <InferJobButton onClick={handleInferJob} pending={inferPending} defaultText='Re-Infer Job' />
-                                </div >
+                        <div className='mt-4'>
+                            <InferJobButton onClick={handleInferJob} pending={inferPending} defaultText='Re-Infer Job' />
+                        </div >
 
-                            </div>
-                    }
-                    <h2 className='text-3xl font-bold mt-8 dark:text-white'>
-                        Inferred Job Match
-                    </h2>
-                    {
-
-                        !inferredJobMatch || Object.keys(inferredJobMatch).length === 0 ? (
-                            <>
-                                <div className='mt-4'>
-                                    <input type='hidden' value={jobDescription} name='jobDescription' />
-
-                                    <InferJobMatchButton pending={inferMatchPending} onClick={handleInferJobMatch} />
-                                </div >
-
-                            </>
-
-                        ) :
-                            <div className='mt-4'>
-                                <InferredData keyName='Match Percentage' value={`${inferredJobMatch.compatibilityMatrix.matchPercentage}%`} />
-                                <InferredData keyName='Match Reason' value={inferredJobMatch.compatibilityMatrix.matchReason} />
-                                <InferredData keyName="Required Skills" value="" />
-
-                                <InferredData keyName='Technical Skills' value={inferredJobMatch.compatibilityMatrix.requiredSkills.techSkills.join(", ")} />
-                                <InferredData keyName='Soft Skills' value={inferredJobMatch.compatibilityMatrix.requiredSkills.softSkills.join(", ")} />
-
-
-                                <div className='mt-4'>
-                                    <InferJobMatchButton pending={inferMatchPending} onClick={handleInferJobMatch} defaultText='Re-Infer Match' />
-                                </div >
-                                <div className="mt-5">
-                                    <span className="text-md">Want to infer another job? Go to <a className="border-b-2" href='/'>Home</a> to enter a new job description.</span>
-                                    <p></p>
-                                    <p>Or</p>
-                                    <AddJobButton job={job} jobDescription={jobDescription} match={match} />
-                                </div>
-
-                            </div>
-                    }
-                    <span className="text-md">Want to infer another job? Go to <a className="border-b-2" href='/'>Home</a> to enter a new job description.</span>
-
-                </>
+                    </div>
             }
+            <h2 className='text-3xl font-bold mt-8 dark:text-white'>
+                Inferred Job Match
+            </h2>
+            {
+
+                !inferredJobMatch || Object.keys(inferredJobMatch).length === 0 ? (
+                    <>
+                        <div className='mt-4'>
+                            <input type='hidden' value={jobDescription} name='jobDescription' />
+
+                            <InferJobMatchButton pending={inferMatchPending} onClick={handleInferJobMatch} />
+                        </div >
+
+                    </>
+
+                ) :
+                    <div className='mt-4'>
+                        <InferredData keyName='Match Percentage' value={`${inferredJobMatch.compatibilityMatrix.matchPercentage}%`} />
+                        <InferredData keyName='Match Reason' value={inferredJobMatch.compatibilityMatrix.matchReason} />
+                        <InferredData keyName="Required Skills" value="" />
+
+                        <InferredData keyName='Technical Skills' value={inferredJobMatch.compatibilityMatrix.requiredSkills.techSkills.join(", ")} />
+                        <InferredData keyName='Soft Skills' value={inferredJobMatch.compatibilityMatrix.requiredSkills.softSkills.join(", ")} />
+
+
+                        <div className='mt-4'>
+                            <InferJobMatchButton pending={inferMatchPending} onClick={handleInferJobMatch} defaultText='Re-Infer Match' />
+                        </div >
+                        <div className="mt-5">
+                            <span className="text-md">Want to infer another job? Go to <a className="border-b-2" href='/'>Home</a> to enter a new job description.</span>
+                            <p></p>
+                            <p>Or</p>
+                            <AddJobButton job={job} jobDescription={jobDescription} match={match} />
+                        </div>
+
+                    </div>
+            }
+            <span className="text-md">Want to infer another job? Go to <a className="border-b-2" href='/'>Home</a> to enter a new job description.</span>
 
         </>
+
+
+
     )
 }
